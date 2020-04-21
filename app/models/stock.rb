8d706603018017aec52683c423d3c0a67b4238ef
Rohit Ghotkar <rohitghotkar123@gmail.com>
2020-04-22 02:19:14 +0530

    Search and display stock price in browser

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,7 +4,6 @@ class Stock < ApplicationRecord
     # client = IEX::Api::Client.new(publishable_token: 'Tpk_9d7f945f4edd48e3a87e69cdce4dc309',
     client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key] ,
                                   endpoint: 'https://sandbox.iexapis.com/v1')
-    
     client.price(ticker_symbol)
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'stocks/search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#index'
   devise_for :users
   get 'my_portfolio', to: 'users#my_portfolio'  
+  get 'search_stock', to: 'stocks#search'
 end
