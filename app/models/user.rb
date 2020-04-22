29b519c9f026f66c6272bcfeb09e069682642ec6
Rohit Ghotkar <rohitghotkar123@gmail.com>
2020-04-22 12:44:34 +0530

    Tasks:      1. Generated Friendship Model      2. Self referential association of users table for friendships

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
 
+  # self referential association
+  has_many :friendships
+  has_many :friends, through: :friendships
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
